c92a828dccb47bdcefc6cca499da76d44b7b7007
madhater <mad-hater-dev@outlook.com>
2019-04-06 10:53:57 -0500

    Updated logos + universal interceptor to support server side rendering

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import { UniversalInterceptor } from './universal-interceptor';
+
 
 @NgModule({
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: UniversalInterceptor,
+    multi: true
+  }],
   imports: [
     AppModule,
     ServerModule,
